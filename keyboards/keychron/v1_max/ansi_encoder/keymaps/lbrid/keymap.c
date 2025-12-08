e12f8e830abb34f59dd808a8ea962703119199f7
lbrid <lb78654@proton.me>
2025-12-08 23:46:06 +1100

    Tweaks

--- keyboards/keychron/v1_max/ansi_encoder/keymaps/lbrid/keymap.c
+++ keyboards/keychron/v1_max/ansi_encoder/keymaps/lbrid/keymap.c
@@ -45,7 +45,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [WIN_BASE] = LAYOUT_ansi_82(
         KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_DEL,             KC_MUTE,
-        KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     TD(TD_EN_DASH),KC_EQL,KC_BSPC,          KC_PGUP,
+        KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,            KC_PGUP,
         KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,            KC_PGDN,
         KC_LCTL,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,            KC_ENT,             TD(TD_HOME_END),
         KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,            KC_RSFT,  KC_UP,
@@ -77,13 +77,17 @@ void leader_start_user(void) {
 }
 
 void leader_end_user(void) {
-    // Leader + S = print email signature - Default ("Kind Regards")
+    // Leader + S = send default signature - "Kind Regards"
     if (leader_sequence_one_key(KC_S)) {
         SEND_STRING("Kind regards\nLouis\n");
     }
-    // Leader + S + T = print email signature - "Thanks"
+    // Leader + S + T = send "Thanks" signature
     if (leader_sequence_two_keys(KC_S, KC_T)) {
         SEND_STRING("Thanks\nLouis\n");
+    }
+    // Leader + S + P = send "Privileged & Confidential"
+    if (leader_sequence_two_keys(KC_S, KC_P)) {
+        SEND_STRING("Privileged & Confidential");
     }
     // Leader + L = select line
     if (leader_sequence_one_key(KC_L)) {
