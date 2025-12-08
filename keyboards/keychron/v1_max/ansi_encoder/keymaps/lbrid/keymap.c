ae7965c37593752438f5647a4e85c559f0d8ad99
lbrid <lb78654@proton.me>
2025-12-08 23:35:38 +1100

    Updated keymaps, removed combo for tapdance

--- keyboards/keychron/v1_max/ansi_encoder/keymaps/lbrid/keymap.c
+++ keyboards/keychron/v1_max/ansi_encoder/keymaps/lbrid/keymap.c
@@ -1,25 +1,22 @@
-/* Copyright 2023 @ Keychron (https://www.keychron.com)
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 #include QMK_KEYBOARD_H
 #include "keychron_common.h"
 
-// -------------- PROTOTYPES AND ENUMS -------------- //
+// -------------- KEYCODES -------------- //
 
-char *get_date(void);
+// enum custom_keycodes {
+//     EN_DASH = SAFE_RANGE,
+// };
+
+// Tapdance
+enum {
+    TD_HOME_END,
+};
+
+tap_dance_action_t tap_dance_actions[] = {
+    [TD_HOME_END] = ACTION_TAP_DANCE_DOUBLE(KC_HOME, KC_END),
+};
+
+// -------------- KEYMAPS -------------- //
 
 enum layers {
     MAC_BASE,
@@ -27,13 +24,6 @@ enum layers {
     WIN_BASE,
     WIN_FN,
 };
-
-enum custom_keycodes {
-    EN_DASH = SAFE_RANGE,
-    EM_DASH,
-};
-
-// -------------- KEYMAP LAYERS -------------- //
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -55,9 +45,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [WIN_BASE] = LAYOUT_ansi_82(
         KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_DEL,             KC_MUTE,
-        KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,            KC_PGUP,
+        KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     TD(TD_EN_DASH),KC_EQL,KC_BSPC,          KC_PGUP,
         KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,            KC_PGDN,
-        KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,            KC_ENT,             KC_HOME,
+        KC_LCTL,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,            KC_ENT,             TD(TD_HOME_END),
         KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,            KC_RSFT,  KC_UP,
         KC_LCTL,  KC_LGUI,  KC_LALT,                                KC_SPC,                                 QK_LEAD, MO(WIN_FN),KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
 
@@ -80,28 +70,20 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
 };
 #endif // ENCODER_MAP_ENABLE
 
-// -------------- COMBOS AND MACROS -------------- //
+// -------------- LEADER -------------- //
 
-// Combos
-const uint16_t PROGMEM two_home[]    = {KC_HOME, KC_HOME, COMBO_END};
-const uint16_t PROGMEM two_minus[]   = {KC_MINS, KC_MINS, COMBO_END};
-const uint16_t PROGMEM three_minus[] = {KC_MINS, KC_MINS, KC_MINS, COMBO_END};
-
-combo_t key_combos[] = {
-    COMBO(two_home, KC_END),     // Tap HOME x2 = END
-    COMBO(two_minus, EN_DASH),   // Tap MINUS x2 = EN_DASH
-    COMBO(three_minus, EM_DASH), // Tap MINUS x3 = EM_DASH
-};
-
-// Leader shortcuts
 void leader_start_user(void) {
     // TODO: Add any functionality when leader is pressed
 }
 
 void leader_end_user(void) {
-    // Leader + S = print email signature
+    // Leader + S = print email signature - Default ("Kind Regards")
     if (leader_sequence_one_key(KC_S)) {
         SEND_STRING("Kind regards\nLouis\n");
+    }
+    // Leader + S + T = print email signature - "Thanks"
+    if (leader_sequence_two_keys(KC_S, KC_T)) {
+        SEND_STRING("Thanks\nLouis\n");
     }
     // Leader + L = select line
     if (leader_sequence_one_key(KC_L)) {
@@ -115,42 +97,23 @@ void leader_end_user(void) {
     if (leader_sequence_one_key(KC_J)) {
         SEND_STRING(SS_TAP(X_END) SS_TAP(X_DEL) SS_TAP(X_SPC));
     }
-    // Leader + D = print date
-    if (leader_sequence_one_key(KC_D)) {
-        SEND_STRING(get_date());
-    }
-    // Leader + D + T = print date and time
-    if (leader_sequence_two_keys(KC_D, KC_T)) {
-        SEND_STRING(get_date());
-        SEND_STRING(__TIME__);
-    }
 }
 
-// Process macros
+// -------------- PROCESS MACROS -------------- //
+
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     if (!process_record_keychron_common(keycode, record)) {
         return false;
     }
 
-    switch (keycode) {
-        case EN_DASH:
-            if (record->event.pressed) {
-                SEND_STRING("–");
-            }
-            break;
-        case EM_DASH:
-            if (record->event.pressed) {
-                SEND_STRING("—");
-            }
-            break;
-    }
+    // switch (keycode) {
+    //     case EN_DASH:
+    //         if (record->event.pressed) {
+    //             // Windows keypad codes for en-dash
+    //             SEND_STRING(SS_LALT(SS_TAP(X_KP_0) SS_TAP(X_KP_1) SS_TAP(X_KP_5) SS_TAP(X_KP_0)));
+    //         }
+    //         break;
+    // }
 
     return true;
-}
-
-// -------------- HELPER FUNCTIONS -------------- //
-
-char *get_date() {
-    static char date[] = {__DATE__[4], __DATE__[5], ' ', __DATE__[0], __DATE__[1], __DATE__[2], ' ', __DATE__[7], __DATE__[8], __DATE__[9], __DATE__[10], '\0'};
-    return date;
 }
